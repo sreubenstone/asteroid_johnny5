90f2d74a32d918a4aab86950749343d5dec7ee2d
Steven Reubenstone <Crossover@Stevens-MacBook-Air-3.local>
2018-11-28 20:01:16 -0500

    working on algorithm

---
+++ algo.js
@@ -0,0 +1,18 @@
+/*
+
+-- Get closest object to Earth at time of test (Random out of 8 returned)
+Size of Asteroid
+Velocity of Asteroid
+Miss Distnace (Earth)
+Miss Distance (Moon)
+
+
+
+
+INPUT
+
+-- Store an array for each quadrant every quarter second [1, 1, 3, 4, 2 1]
+-- 
+
+
+*/
