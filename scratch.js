4b10fed661d6022879d50a4823aa21f5255d0d33
Steven Reubenstone <Crossover@Stevens-MacBook-Air-3.local>
2018-11-28 19:10:01 -0500

    saving

---
+++ scratch.js
@@ -0,0 +1,77 @@
+{
+    "links": {
+        "self": "https://api.nasa.gov/neo/rest/v1/neo/2217807?api_key=DEMO_KEY"
+    },
+    "id": "2217807",
+        "neo_reference_id": "2217807",
+            "name": "217807 (2000 XK44)",
+                "nasa_jpl_url": "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=2217807",
+                    "absolute_magnitude_h": 18.1,
+                        "estimated_diameter": {
+        "kilometers": {
+            "estimated_diameter_min": 0.6376097899,
+                "estimated_diameter_max": 1.4257388333
+        },
+        "meters": {
+            "estimated_diameter_min": 637.6097898754,
+                "estimated_diameter_max": 1425.7388332807
+        },
+        "miles": {
+            "estimated_diameter_min": 0.3961922327,
+                "estimated_diameter_max": 0.8859127646
+        },
+        "feet": {
+            "estimated_diameter_min": 2091.8957030147,
+                "estimated_diameter_max": 4677.6209937807
+        }
+    },
+    "is_potentially_hazardous_asteroid": false,
+        "close_approach_data": [
+            {
+                "close_approach_date": "2018-11-28",
+                "epoch_date_close_approach": 1543392000000,
+                "relative_velocity": {
+                    "kilometers_per_second": "14.5052360031",
+                    "kilometers_per_hour": "52218.8496110588",
+                    "miles_per_hour": "32446.7624152715"
+                },
+                "miss_distance": {
+                    "astronomical": "0.3489762043",
+                    "lunar": "135.751739502",
+                    "kilometers": "52206096",
+                    "miles": "32439364"
+                },
+                "orbiting_body": "Earth"
+            }
+        ],
+            "orbital_data": {
+        "orbit_id": "68",
+            "orbit_determination_date": "2018-11-24 07:11:52",
+                "first_observation_date": "1975-07-16",
+                    "last_observation_date": "2018-11-09",
+                        "data_arc_in_days": 15822,
+                            "observations_used": 324,
+                                "orbit_uncertainty": "0",
+                                    "minimum_orbit_intersection": ".0724324",
+                                        "jupiter_tisserand_invariant": "4.060",
+                                            "epoch_osculation": "2458600.5",
+                                                "eccentricity": ".3855926391899071",
+                                                    "semi_major_axis": "1.723968516924459",
+                                                        "inclination": "11.2388219354069",
+                                                            "ascending_node_longitude": "48.12351758285045",
+                                                                "orbital_period": "826.7846835710242",
+                                                                    "perihelion_distance": "1.059218946603247",
+                                                                        "perihelion_argument": "347.6374449058367",
+                                                                            "aphelion_distance": "2.388718087245671",
+                                                                                "perihelion_time": "2458440.579832599947",
+                                                                                    "mean_anomaly": "69.63271261310607",
+                                                                                        "mean_motion": ".4354217091263696",
+                                                                                            "equinox": "J2000",
+                                                                                                "orbit_class": {
+            "orbit_class_type": "AMO",
+                "orbit_class_description": "Near-Earth asteroid orbits similar to that of 1221 Amor",
+                    "orbit_class_range": "1.017 AU < q (perihelion) < 1.3 AU"
+        }
+    },
+    "is_sentry_object": false
+},
