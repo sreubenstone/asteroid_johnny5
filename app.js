cc9743852cbb4a2868ffdcce1deedcc502b19ca6
Steven Reubenstone <Crossover@Stevens-MacBook-Air-3.local>
2018-11-29 18:26:59 -0500

    messing around with johnny five

--- app.js
+++ app.js
@@ -4,8 +4,72 @@ const executeHoroscope = require('./algo')
 const accountSid = 'AC4f4a47682dd13eee8b49aa22c3538fe4';
 const authToken = 'bfc9ff64caca3c90a9e58b786316e5c8';
 const client = require('twilio')(accountSid, authToken);
+const five = require('johnny-five');
+const raspi = require('raspi-io');
 
-// should be able to complete all the way down to horoscope and sms
+const board = new five.Board({
+    io: new raspi(),
+});
+
+board.on("ready", function () {
+
+    // Create a new `joystick` hardware instance.
+    var joystick = new five.Joystick({
+        //   [ x, y ]
+        pins: ["A0", "A1"]
+    });
+
+    joystick.on("change", function () {
+        console.log("Joystick");
+        console.log("  x : ", this.x);
+        console.log("  y : ", this.y);
+        console.log("--------------------------------------");
+    });
+});
+
+
+
+
+/* Autoboot Server (Npm run dev this directory on pi start)
+
+LCD: "Press red button to begin"...user presses button
+
+Program runs and collects input on 1 second interval for 10 seconds
+
+LCD should show somefeedback of the joy stick
+
+Input is saved...
+
+
+
+Move stick for 7 seconds
+
+Stores data
+
+Displays fortune
+
+Asks for phone number
+
+Sends fortune  
+https://www.crowdsupply.com/arsenijs/zerophone
+
+STEP 1
+
+Node Server
+    Receive data
+        Movement Information
+        Sender Information
+
+                            Process Data
+                                    Asteroid https://api.nasa.gov/api.html#neows-swagger
+                                    
+            
+
+Output data
+    Response
+
+
+*/
 
 const input = [1, 1, 3, 4, 2, 1, 1, 3, 4, 2]
 
