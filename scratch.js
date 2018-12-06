a4b7ffe8253f3c4ba070959ffc1cabb36b8b2bb9
Steven Reubenstone <steven@collaborizm.com>
2018-12-06 17:46:50 -0500

    working on j5

--- scratch.js
+++ scratch.js
@@ -15,6 +15,26 @@ board.on("ready", function () {
         console.log("  x : ", this.x);
         console.log("  y : ", this.y);
         console.log("--------------------------------------");
+		// quadrant one x is postive and y is negative
+		// quadrant two x is negative and y is negative
+		// quadrant three x is negative and y is positive
+		// quadrant four x is positive and y is positive
+		if (this.x > 0 && this.y < 0) {
+			const input = 1
+			console.log('quadrant:', input)
+		} else if (this.x < 0 && this.y < 0) {
+			const input = 2
+			console.log('quadrant:', input)
+		} else if (this.x < 0 && this.y > 0) {
+			const input = 3
+			console.log('quadrant:', input)
+		} else {
+			const input = 4	
+			console.log('quadrant:', input)
+		}
+			
+		joystick.off()
+		
     });
 });
 
